c153825529356938e2c796ee2a12b768baf1647d
Esteban-Alvarez <84643021+Esteban-Alvarez@users.noreply.github.com>
2025-09-09 00:11:52 -0500

    Added Item class

---
+++ app/Models/Item.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Item extends Model
+{
+    /**
+     * ITEM ATTRIBUTES
+     * $this->attributes['id'] - int - contains the item primary key (id)
+     * $this->attributes['quantity'] - int - contains the item quantity
+     * $this->attributes['price'] - int - contains the item price
+     * $this->attributes['created_at'] - string - contains the timestamp of creation
+     * $this->attributes['updated_at'] - string - contains the timestamp of updates
+     * this->order - Order- contains the associated order
+     * this->product - Product - contains the associated product
+     */
+    protected $fillable = ['quantity', 'price'];
+
+    public static function validate($request): void
+    {
+        $request->validate([
+            'quantity' => 'required|integer|min:1',
+            'price' => 'required|integer|min:0',
+        ]);
+    }
+
+    public function getId(): int
+    {
+        return $this->attributes['id'];
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->attributes['quantity'];
+    }
+
+    public function getPrice(): float
+    {
+        return $this->attributes['price'];
+    }
+
+    public function setQuantity(int $quantity): void
+    {
+        $this->attributes['quantity'] = $quantity;
+    }
+
+    public function setPrice(float $price): void
+    {
+        $this->attributes['price'] = $price;
+    }
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function getOrder(): Collection
+    {
+        return $this->order;
+    }
+
+    public function setOrder(Collection $order): void
+    {
+        $this->order = $order;
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function getProduct(): Collection
+    {
+        return $this->product;
+    }
+
+    public function setProduct(Collection $product): void
+    {
+        $this->products = $product;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 $CUSTOMER = 'App\Http\Controllers\CustomerController';
+$ITEM = 'App\Http\Controllers\ItemController';
 
 Route::get('/customer', $CUSTOMER.'@index')->name('customer.index');
 Route::get('/customer/create', $CUSTOMER.'@create')->name('customer.create');
 Route::post('/customer/save', $CUSTOMER.'@save')->name('customer.save');
 Route::get('/customer/{id}', $CUSTOMER.'@show')->name('customer.show');
 Route::delete('/customer/delete/{id}', $CUSTOMER.'@delete')->name('customer.delete');
+
+Route::get('/item', $ITEM.'@index')->name('item.index');
+Route::get('/item/create', $ITEM.'@create')->name('item.create');
+Route::post('/item/save', $ITEM.'@save')->name('item.save');
+Route::get('/item/{id}', $ITEM.'@show')->name('item.show');
+Route::delete('/item/delete/{id}', $ITEM.'@delete')->name('item.delete');
