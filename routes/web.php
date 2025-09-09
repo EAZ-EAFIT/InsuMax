8cd71035e376bde95972e22136107d8e80951f84
mpinedaa4 <mpinedaa4@eafit.edu.co>
2025-09-09 13:28:34 -0500

    Added the Product class

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+$PRODUCT_CONTROLLER_PATH = 'App\Http\Controllers\ProductController';
+
+Route::get('/products', $PRODUCT_CONTROLLER_PATH.'@index')->name('product.index');
+Route::get('/products/{id}', $PRODUCT_CONTROLLER_PATH.'@show')->name('product.show');
