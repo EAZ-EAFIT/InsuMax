a0909800bc1769c3a8a985b1d9c26cf17731aa19
mpinedaa4 <mpinedaa4@eafit.edu.co>
2025-09-11 18:58:19 -0500

    First advance in frontend

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,10 +1,33 @@
-@extends('layouts.app')
-@section('title', 'Home')
-@section('content')
-    <header>
-        <h1>Home</h1>
-    </header>
-    <div>
-        Welcome to the application
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('/css/general.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/home/home.css') }}">
+  <title>{{ __('home/index.title') }}</title>
+</head>
+<body class="flex column">
+  <header class="flex">
+    <h1 class="logo dark-blue">
+      {{ __('home/index.logoStart') }}<span class="logo-span">{{ __('home/index.logoEnd') }}</span>
+    </h1>
+
+    <a href="" class="btn-dark-blue login-btn">{!! __('home/index.login') !!}</a>
+  </header>
+
+  <main class="flex">
+    <div class="home-container flex column">
+      <h2 class="white slogan">{{ __('home/index.slogan') }}</h2>
+      <p class="white cta">{{ __('home/index.callToAction') }}</p>
+      <a href="{{ route('product.index') }}" class="btn-dark-blue cta-btn">{{ __('home/index.callToActionButton') }}</a>
     </div>
-@endsection
+  </main>
+</body>
+</html>
