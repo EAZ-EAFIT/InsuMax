9dc4ac3b540170cf410c2d8d70c94461d3fff109
mpinedaa4 <mpinedaa4@eafit.edu.co>
2025-10-30 21:17:30 -0500

    Unit tests created for the Product model

---
+++ tests/Unit/ProductTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\Product;
+
+class ProductTest extends TestCase
+{
+    public function test_set_name_and_get_name()
+    {
+        $product = new Product();
+
+        $product->setName('Laptop');
+        $this->assertEquals('Laptop', $product->getName());
+    }
+
+    public function test_set_price_and_get_price()
+    {
+        $product = new Product();
+
+        $product->setPrice(15000);
+        $this->assertEquals(15000, $product->getPrice());
+    }
+}
