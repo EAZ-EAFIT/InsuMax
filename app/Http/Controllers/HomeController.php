f5058e1062903a281591fbe53596d5183ad486b7
mpinedaa4 <mpinedaa4@users.noreply.github.com>
2025-11-08 22:14:03 +0000

    PHP Linting (Pint)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,14 +9,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
-use Illuminate\View\View;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
     public function index(): View|RedirectResponse
     {
-        if (Auth::check()){
+        if (Auth::check()) {
             return redirect()->route('product.index');
         }
 
