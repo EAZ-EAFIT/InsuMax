28689abb32bdbd15721de2e92b48aec040a1ee4a
mpinedaa4 <mpinedaa4@eafit.edu.co>
2025-11-09 23:23:16 -0500

    Adjusted the frontend of the allied products

--- resources/lang/en/allied/index.php
+++ resources/lang/en/allied/index.php
@@ -2,5 +2,8 @@
 
 return [
     'title' => 'Allied Products',
+    'store' => 'Cellhub',
     'no_products' => 'No allied products found',
+    'storeLink' => 'http://cellhub.store/',
+    'goToStore' => 'Click here to visit the store\'s website',
 ];
